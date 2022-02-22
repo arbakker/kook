86a8bdd0e02a8b52b559e2ae3606e53235e970f0
Anton Bakker <a.r.bakker1@gmail.com>
2022-02-22 20:41:19 +0100

    improved footer

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -3,7 +3,34 @@
     <v-main>
      <router-view></router-view>
     </v-main>
+    <v-footer
+      padless
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="grey darken-2' text-center"
+      >
+        <v-card-text>
+          <v-btn
+            href="https://github.com/arbakker/kook"
+            target="_blank"
+            class="mx-4"
+            icon
+          >
+            <v-icon size="24px">
+              mdi-github
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+        <v-card-text>
+          KOOK - <a href="https://github.com/arbakker">arbakker's</a> persoonlijke kookboek
+        </v-card-text>
+      </v-card>
+  </v-footer>
   </v-app>
+  
 </template>
 
 <script>
