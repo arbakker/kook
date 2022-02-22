fe619a5d9e12c6b44fa6600ab4f4378647be251a
Anton Bakker <a.r.bakker1@gmail.com>
2022-02-22 19:59:39 +0100

    disable source maps

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: './',
+  productionSourceMap: process.env.NODE_ENV != 'production',
   transpileDependencies: [
     'vuetify'
   ]
