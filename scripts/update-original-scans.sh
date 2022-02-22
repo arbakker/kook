5c9e149467907f2169f1fb1cdae12a3399e8417d
arbakker <a.r.bakker1@gmail.com>
2022-02-22 18:57:40 +0000

    Deploying to gh-pages from  @ e0c8a632d840f5462b04df83636a6f496504e719 🚀

--- scripts/update-original-scans.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "RUNNING: ${0}"
-latest_zip=$(find ~/Downloads/ -name "kook*.zip" -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
-script_path=`realpath $0`
-script_dir=`dirname $script_path`
-orig_scans_dir="${script_dir}/../data/input/scans-originals"
-rm -f $orig_scans_dir/*
-unzip -o "$latest_zip" -d "$orig_scans_dir"
-$script_dir/refresh-scans.sh
